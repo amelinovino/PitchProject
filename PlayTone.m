b5041ea9f9bc4002715a4ab6d0b1e5f275819a53
Amelie Harder <amphie@MacBook-Air-16.local>
2024-11-11 12:20:49 +0000

    second try to commit

--- PlayTone.m
+++ PlayTone.m
@@ -1,7 +1,7 @@
 function PlayTone(frequency)
 
 s = 8000; % sampling rate
-d = 0.8; % duration 
+d = 2.7; % duration 
 n = d*s; % number of samples
 t = 1:n; 
 tone = sin(2*pi*frequency*t/s);
--- RunTrial.m
+++ RunTrial.m
@@ -3,11 +3,11 @@ function [isCorrect] = RunTrial(fLow, fHigh)
 randomOrder = randperm(2);
 if randomOrder(1)==1
     PlayTone(fLow);
-    pause
+    pause(0.8)
     PlayTone(fHigh);
 else
     PlayTone(fHigh);
-    pause
+    pause(0.8)
     PlayTone(fLow);
 end
 response = input ('Which tone was lower frequency, first (1) or second (2)? ');
